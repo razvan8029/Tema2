b9f51172772d80025433d61bbc6f14d87891bd5d
razvanlutac <r.lutac01@gmail.comgit config --global user.email r.lutac01@gmail.com>
2021-05-05 20:19:03 +0300

    Add project files.

---
+++ Data/MovieApplication1Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using WebApplication1.Models;
+
+namespace MovieApplication1.Data
+{
+    public class MovieApplication1Context : DbContext
+    {
+        public MovieApplication1Context(DbContextOptions<MovieApplication1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication1.Models.Movie> Movie { get; set; }
+    }
+}
